44456b0d480d75d561eeee7e369db432f6e94405
ProGM <progiemmeh@gmail.com>
2014-03-03 23:01:13 +0100

    Implemented *.text edit feature and fixed bugs
    Changes:
    -Now when you have multiple textfields you don't have conflicts
    -Now events are much lower
    -Implemented the possibility to edit the .text value in real time and see the results on the screen

--- win_fix.lua
+++ win_fix.lua
@@ -1,72 +1,121 @@
+local _SELECTED = nil
+
+
 function native.newTextField( left, top, width, height, listener )
 	local group = display.newGroup( )
 	group._selected = false
-	group.text = ""
 	local element = display.newText{
 		text = "",
 		x = left, y = top,
 		width = width, height = height,
+		fontSize=16
 	}
-	element:setFillColor( 1, 1, 1 )
+	element:setFillColor( 0, 0, 0 )
 	local bkg = display.newRect( left, top, width, height )
 	bkg:setFillColor(0.5, 0.5, 0.5)
+	bkg:setStrokeColor( 0, 0, 0 )
+	bkg.strokeWidth = 1
 	group:insert(bkg)
 	group:insert(element)
 	function group:tap(e)
-		bkg:setFillColor(0.3, 0.3, 0.3)
-		group._selected = true
+		if _SELECTED ~= nil then
+			_SELECTED:_setSelected(false)
+		end
+		self:_setSelected(true)
 		local event = {name="userInput", target=group, phase="began"}
 		group:dispatchEvent(event)
 		return true
 	end
 	group:addEventListener( "tap", group )
-	local function onKeyEvent( event )
-		if not group._selected then return false end
-		if event.phase == "up" then return false end
-		if event.keyName == "deleteBack" then
-			group.text = group.text:sub(0, #group.text - 1)
-			element.text = group.text
-			return true
-		end			if event.keyName == "space" then
-		event.keyName = " "
-		end
-		if event.keyName:len() > 1 then
-			return false
-		end
-		if event.isShiftDown then
-			event.keyName = event.keyName:upper()
-		end
 
-		group.text = group.text .. event.keyName
-		element.text = group.text
+	function group:_setSelected(bool)
+		if bool then
+			bkg:setFillColor(1, 1, 1)
+			_SELECTED = self
+		else
+			bkg:setFillColor(0.7, 0.7, 0.7)
+			if _SELECTED == self then
+				_SELECTED = nil
+			end
+		end
+		self._selected = bool
+	end
 
-		local event = {name="userInput", target=group, phase="editing", newCharacters = event.keyName}
-		group:dispatchEvent(event)
-	    return true
-	end
-	Runtime:addEventListener( "key", onKeyEvent )
-
-	local function disableTap(e)
-		bkg:setFillColor(0.5, 0.5, 0.5)
-		group._selected = false
-		local event = {name="userInput", target=group, phase="ended"}
-		group:dispatchEvent(event)
-		return true
-	end
-	if listener ~= nil then
-		group:addEventListener( "userInput", listener )
-	end
-	Runtime:addEventListener( "tap", disableTap )
 	group._old_removeSelf = group.removeSelf
 
 	function group:removeSelf()
-		Runtime:removeEventListener("tap", disableTap)
-		Runtime:removeEventListener( "key", onKeyEvent )
+		self:_setSelected(false)
 		self:removeEventListener("tap", listener)
 		self:_old_removeSelf()
 	end
+
+	if listener ~= nil then
+		group:addEventListener( "userInput", listener )
+	end
+
+	local mt = getmetatable( group )
+	local newMT = {}
+	for k,v in pairs(mt) do
+		newMT[k] = v
+	end
+	local oldn = newMT.__newindex
+	local old = newMT.__index
+	function newMT.__index(table, key)
+		if key == "text" then
+			print("sto modificando text")
+			return element.text
+		end
+		return old(table, key)
+	end
+	function newMT.__newindex(table, key, value)
+		if key == "text" and value then
+			print(key, value)
+			element.text = value
+			table[key] = nil
+			return false
+		end
+		oldn(table, key, value)
+	end
+	setmetatable( group, newMT )
+
 	return group
 end
 function native.newTextBox( left, top, width, height, listener )
 	return native.newTextField( left, top, width, height, listener )
 end
+
+
+local function onKeyEvent( event )
+	if _SELECTED == nil then return false end
+	if event.phase == "up" then return false end
+	if event.keyName == "deleteBack" then
+		_SELECTED.text = _SELECTED.text:sub(0, #_SELECTED.text - 1)
+		return true
+	end
+	if event.keyName == "space" then
+		event.keyName = " "
+	end
+	if event.keyName:len() > 1 then
+		return false
+	end
+	if event.isShiftDown then
+		event.keyName = event.keyName:upper()
+	end
+
+	_SELECTED.text = _SELECTED.text .. event.keyName
+
+	local event = {name="userInput", target=_SELECTED, phase="editing", newCharacters = event.keyName}
+	_SELECTED:dispatchEvent(event)
+    return true
+end
+Runtime:addEventListener( "key", onKeyEvent )
+
+local function disableTap(e)
+	if _SELECTED == nil then return false end
+	local event = {name="userInput", target=_SELECTED, phase="ended"}
+	_SELECTED:dispatchEvent(event)
+	_SELECTED:_setSelected(false)
+	return true
+end
+Runtime:addEventListener( "tap", disableTap )
+
